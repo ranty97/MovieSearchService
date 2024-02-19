6109d241cc4fb89e908d0b77b1e4e2fb0f8db744
Ivan Nikolaev <googacc9091@gmail.com>
2024-02-19 18:20:23 +0300

    Init Commit

---
+++ src/main/java/org/example/moviesearchservice/MovieSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.moviesearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/moviesearchservice/MovieSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moviesearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
